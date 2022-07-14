9c9dfdb5bd6e403e8f35e66b74401cca43500f8a
ge47haz <ge47haz@mytum.de>
2022-07-14 10:53:33 +0200

    blyat

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else {
+        } else if(query.contains("Long ist ein")){
+            return "GOTT";
+        }
+        else {
             return "";
         }
     }
